18095928bf1cf9771f323f6ff8b6fd490c56fa2d
Matheus Lino <7matheuslino@gmail.com>
2020-05-01 17:09:31 -0300

    Finisihing class 'Repositório do TypeORM'

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,30 +1,29 @@
 import { Router, request, response } from 'express'
 import { parseISO } from 'date-fns'
+import { getCustomRepository } from 'typeorm'
 
 import AppointmentsRepository from '../repositories/AppointmentsRepository'
 import CreateAppointmentService from '../services/CreateAppointmentService'
 
 const appointmentRouter = Router()
 
-const appointmentsRepository = new AppointmentsRepository()
+appointmentRouter.get('/', async (request, response) => {
+  const appointmentsRepository = getCustomRepository(AppointmentsRepository)
 
-appointmentRouter.get('/', (request, response) => {
-  const appointments = appointmentsRepository.all()
+  const appointments = await appointmentsRepository.find()
 
   return response.json(appointments)
 })
 
-appointmentRouter.post('/', (request, response) => {
+appointmentRouter.post('/', async (request, response) => {
   try {
     const { provider, date } = request.body
 
     const parsedDate = parseISO(date)
 
-    const createAppointmentService = new CreateAppointmentService(
-      appointmentsRepository
-    )
+    const createAppointmentService = new CreateAppointmentService()
 
-    const appointment = createAppointmentService.execute({
+    const appointment = await createAppointmentService.execute({
       provider,
       date: parsedDate
     })
