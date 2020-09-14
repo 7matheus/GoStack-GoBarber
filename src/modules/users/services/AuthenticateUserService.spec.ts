bb01e450e906e0bc87448493f20f3b6d1a20af56
Matheus Lino <7matheuslino@gmail.com>
2020-09-14 20:30:49 -0300

    Finalizing many classes about TDD.

---
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -0,0 +1,34 @@
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import FakeHashProvider from '@modules/users/providers/HashProvider/fakes/FakeHashProvider';
+import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
+import CreateUserService from '@modules/users/services/CreateUserService';
+
+describe('AuthenticateUser', () => {
+  it('should be able to authenticate', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider
+    );
+    const authenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider
+    );
+
+    const user = await createUser.execute({
+      name: 'John Doe',
+      email: 'johndoe@gmail.com',
+      password: '1234567'
+    });
+
+    const response = await authenticateUser.execute({
+      email: 'johndoe@gmail.com',
+      password: '1234567'
+    });
+
+    expect(response).toHaveProperty('token');
+    expect(response.user).toEqual(user);
+  });
+});
