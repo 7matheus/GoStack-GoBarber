5b22c4c8240fe4c151a8ae0eb7d2cf9859b41fbf
Matheus Lino <7matheuslino@gmail.com>
2020-11-05 18:28:09 -0300

    Finalizing class 'Criação do agendamento'

--- src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
+++ src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -50,6 +50,7 @@ class AppointmentsRepository implements IAppointmentsRepository {
 
   public async create({
     provider_id,
+    user_id,
     date
   }: ICreateAppointmentDTO): Promise<Appointment> {
     const appointment = new Appointment();
@@ -58,7 +59,7 @@ class AppointmentsRepository implements IAppointmentsRepository {
     // appointment.id = uuid();
     // appointment.date = date;
     // appointment.provider_id = provider_id;
-    Object.assign(appointment, { id: uuid(), date, provider_id });
+    Object.assign(appointment, { id: uuid(), date, provider_id, user_id });
 
     this.appointments.push(appointment);
 
