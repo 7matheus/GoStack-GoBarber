4fa47125bd1263e1c1c7b2fb679e8d3961eaadc5
Matheus Lino <7matheuslino@gmail.com>
2020-11-05 19:29:11 -0300

    Finalizing class 'Rotas e controllers'

--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -2,12 +2,24 @@ import { Router } from 'express';
 
 import ensureAuthenticaded from '@modules/users/infra/http/middleware/ensureAuthenticated';
 import ProvidersController from '@modules/appointments/infra/http/controllers/ProvidersController';
+import ProviderDayAvailabilityController from '../controllers/ProviderDayAvailabilityControllerts';
+import ProviderMounthAvailabilityController from '../controllers/ProviderMounthAvailabilityController';
 
 const providersRouter = Router();
 const providersController = new ProvidersController();
+const providerDayAvailabilityController = new ProviderDayAvailabilityController();
+const providerMounthAvailabilityController = new ProviderMounthAvailabilityController();
 
 providersRouter.use(ensureAuthenticaded);
 
 providersRouter.get('/', providersController.index);
+providersRouter.get(
+  '/:id/mounth-availability',
+  providerDayAvailabilityController.index
+);
+providersRouter.get(
+  '/:id/day-availability',
+  providerMounthAvailabilityController.index
+);
 
 export default providersRouter;
